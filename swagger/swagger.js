bfc58a708f32e0e05501da83b2bcd7f6186e9dce
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-10-29 05:51:25 +0800

    test: render 測試

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: '營養日記 API', // 文件名稱
     description: '營養日記文件' // 文件描述
   },
-  host: 'nutrientbank-app-backend.onrender.com', // (重要) 本地:localhost:3005 | render:https://nutrientbank-app-backend.onrender.com
+  host: 'nutrientbank-backend-app.onrender.com', // (重要) 本地:localhost:3005 | render:https://nutrientbank-app-backend.onrender.com
 	basePath: '', // 預設為'/'
   schemes: ['http', 'https'], // swagger文件支援哪幾種模式
 	securityDefinitions: { // 驗證 token
