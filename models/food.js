cde67d488f31d0427162cb05666c99e845dd3230
ssuyun.chen <ssuyun.chen@udngroup.com>
2022-11-04 17:32:07 +0800

    feat: 修改食品 model

--- models/food.js
+++ models/food.js
@@ -25,20 +25,32 @@ const foodSchema = new mongoose.Schema(
       enum: ['克', '毫升'],
       required: [true, '食品重含量單位未填寫正確']
     },
-    nutrition: [
-      {
-        _id: false,
-        ingredient: {
-          type: String,
-          default: '糖',
-          enum: ['糖','碳水化合物', '反式脂肪', '熱量', '脂肪', '蛋白質', '鈉', '飽和脂肪'],
-        },
-        perUnitContent: {
-          type: Number,
-          default: 0
-        }
-      }
-    ],
+    // nutrition: [
+    //   {
+    //     _id: false,
+    //     ingredient: {
+    //       type: String,
+    //       default: '糖',
+    //       enum: ['糖','碳水化合物', '反式脂肪', '熱量', '脂肪', '蛋白質', '鈉', '飽和脂肪'],
+    //     },
+    //     perUnitContent: {
+    //       type: Number,
+    //       default: 0
+    //     }
+    //   }
+    // ],
+    nutrition: {
+      type: Object,
+      calories: 0,
+      carbohydrates: 0,
+      protein: 0,
+      fat: 0,
+      saturated_fat: 0,
+      trans_fat: 0,
+      sodium: 0,
+      sugar: 0,
+      required: [true, '食品營養成分未填寫正確']
+    },
     createdAt: {
       type: Date,
       default: Date.now(),
--- service/appHelper.js
+++ service/appHelper.js
@@ -46,7 +46,7 @@ exports.generateSendJWT = (user, res) => {
 // TODO 驗證
 exports.verifyFood = (data, next) => {
   let { name, subName, brand, perUnitWeight, unit, nutrition } = data;
-  let ingredientType = ['糖','碳水化合物', '反式脂肪', '熱量', '脂肪', '蛋白質', '鈉', '飽和脂肪'];
+  let ingredientType = ['calories','carbohydrates', 'protein', 'fat', 'saturated_fat', 'trans_fat', 'sodium', 'sugar'];
   const unitType = ['克','毫升'];
   // 食品名稱欄位正確
   if (!name) {
@@ -93,16 +93,20 @@ exports.verifyFood = (data, next) => {
     return appError({statusCode: 400, message:'食品重含量單位未填寫正確(克、毫升)'}, next);
   };
   // 食品營養成分欄位正確
-  if (nutrition.length !== 8) {
-    return appError({statusCode: 400, message:`食品營養成分未填寫正確(${ingredientType})`}, next);
-  } else {
-    nutrition.find((nut) => {
-      if (!ingredientType.includes(nut.ingredient)) {
-        return appError({statusCode: 400, message:`食品營養成分名稱未填寫正確(${ingredientType})`}, next);
-      };
-      if (nut.perUnitContent < 0) {
-        return appError({statusCode: 400, message:'食品營養成分重含量未填寫正確'}, next);
-      };
-    });
-  };
+  // if (nutrition.length !== 8) {
+  //   return appError({statusCode: 400, message:`食品營養成分未填寫正確(${ingredientType})`}, next);
+  // } else {
+  //   nutrition.find((nut) => {
+  //     if (!ingredientType.includes(nut.ingredient)) {
+  //       return appError({statusCode: 400, message:`食品營養成分名稱未填寫正確(${ingredientType})`}, next);
+  //     };
+  //     if (nut.perUnitContent < 0) {
+  //       return appError({statusCode: 400, message:'食品營養成分重含量未填寫正確'}, next);
+  //     };
+  //   });
+  // };
+  const isExist = Object.keys(nutrition).some((ingredientType) => nutrition[ingredientType]);
+  if (!isExist) {
+    return appError({statusCode: 400, message:`食品營養成分名稱未填寫正確(${ingredientType})`}, next);
+  }
 };
